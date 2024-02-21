14135cc53e1c82031eeedece3877171e39c25db5
Gizem ALTINCI <gizemaltnc@outlook.com>
2024-02-21 08:46:37 +0300

    finished

--- Toast_Alert/Toast.js
+++ Toast_Alert/Toast.js
@@ -1,13 +1,26 @@
-const showHide = () => {
-  const toastContainerEl = document.querySelector(".toast_container");
+let toastTimer;
+let toastStatus;
+const toastContainerEl = document.querySelector(".toast_container");
+
+const showHide = (status) => {
+  toastStatus = status;
 
   if (toastContainerEl.classList.contains("hide"))
     toastContainerEl.classList.remove("hide");
 
   toastContainerEl.classList.add("show");
+  toastContainerEl.classList.add(toastStatus);
 
-  setTimeout(() => {
+  toastTimer = setTimeout(() => {
     toastContainerEl.classList.remove("show");
+    toastContainerEl.classList.remove(toastStatus);
     toastContainerEl.classList.add("hide");
   }, 5000);
+};
+
+const closeAlert = () => {
+  clearTimeout(toastTimer);
+  toastContainerEl.classList.remove("show");
+  toastContainerEl.classList.remove(toastStatus);
+  toastContainerEl.classList.add("hide");
 };
